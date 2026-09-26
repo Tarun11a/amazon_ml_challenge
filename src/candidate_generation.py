ab98f59983aea61e660c2082fe14cd469794c147
Tarun11a <YOUR_NEW_GMAIL@gmail.com>
2026-09-26 11:09:17 +0530

    WIP: candidate generation

--- src/candidate_generation.py
+++ src/candidate_generation.py
@@ -3,51 +3,46 @@ candidate_generation.py
 
 Generate candidate_pairs.tsv
 """
-
+print("candidate_generation.py started")
 import pandas as pd
 
-from blocking import generate_candidates
+from blocking import CandidateGenerator
 
 from utils import save_candidate_file
 
 
 def main():
 
-    print("Loading datasets...")
+    print("Loading Test Dataset...")
 
     s1 = pd.read_csv(
-
         "dataset/test/test_source1.tsv",
-
         sep="\t"
-
     )
 
     s2 = pd.read_csv(
-
         "dataset/test/test_source2.tsv",
-
         sep="\t"
-
     )
 
     s3 = pd.read_csv(
-
         "dataset/test/test_source3.tsv",
-
         sep="\t"
-
     )
 
-    print("Generating candidates...")
+    print("Generating Candidate Pairs...")
 
-    candidates = generate_candidates(
+    generator = CandidateGenerator()
+
+    candidates = generator.generate(
 
         s1,
 
         s2,
 
-        s3
+        s3,
+
+        top_k=30
 
     )
 
@@ -58,6 +53,8 @@ def main():
         "output/candidate_pairs.tsv"
 
     )
+
+    print("candidate_pairs.tsv generated successfully.")
 
 
 if __name__ == "__main__":
